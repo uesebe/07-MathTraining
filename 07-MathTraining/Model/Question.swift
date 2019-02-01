858bef874774f172edab82518d4b65d28de156c4
Cristian Torres <ctorres@uesebe.cl>
2019-02-01 18:28:37 -0300

    Initial Commit

---
+++ 07-MathTraining/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  07-MathTraining
+//
+//  Created by Cristian Torres on 1/30/19.
+//  Copyright © 2019 Uesebe. All rights reserved.
+//
+
+struct Question {
+    var text: String
+    var answer: Int
+    var userAnswer: Int?
+}
